a499c458a39eb8dbda213f0d1955ba666e06155c
eltatataa <davidtowo75@gmail.com>
2023-12-22 18:57:46 -0500

    change url api 2

--- frontend/src/utils/const.js
+++ frontend/src/utils/const.js
@@ -1,2 +1,2 @@
 // export const API_URL = "http://localhost:5050";
-export const API_URL = "https://backend-images-cloud.vercel.app/";
+export const API_URL = "https://backend-images-cloud.vercel.app";
