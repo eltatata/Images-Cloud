7469070da56003431ed4533bbdc12992cc6d2f6e
eltatataa <davidtowo75@gmail.com>
2024-07-16 21:21:36 -0500

    auth service

--- backend/src/routes/auth.router.js
+++ backend/src/routes/auth.router.js
@@ -1,12 +1,12 @@
 import express from "express";
 
-import { login, logout, register } from "../controllers/auth.controller.js";
+import { login, register } from "../controllers/auth.controller.js";
 import { loginValidator, registerValidator } from "../middlewares/validator.manager.js";
 
 const router = express.Router();
 
 router.post("/register", registerValidator, register);
 router.post("/login", loginValidator, login);
-router.get("/logout", logout);
+router.get("/profile");
 
 export default router;
