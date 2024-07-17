3ded65eac779c8aa1bd9b2895f922ca4fe25c2e4
eltatataa <davidtowo75@gmail.com>
2024-07-17 06:47:46 -0500

    password utils

--- backend/src/services/auth.service.js
+++ backend/src/services/auth.service.js
@@ -1,11 +1,17 @@
 import jwt from "jsonwebtoken";
 import { User } from '../models/User.js';
+import { hashPassword, comparePassword } from "../utils/password.handler.js";
 
 export const registerService = async (data) => {
   let user = await User.findOne({ email: data.email });
   if (user) throw new Error('El usuario ya existe');
 
-  user = new User(data);
+  user = new User({
+    name: data.name,
+    username: data.username,
+    email: data.email,
+    password: await hashPassword(data.password),
+  });
   await user.save();
 
   return user;
@@ -14,7 +20,7 @@ export const registerService = async (data) => {
 export const loginService = async (data) => {
   const user = await User.findOne({ email: data.email });
   if (!user) throw new Error('Usuario no encontrado');
-  if (!user.comparePassword(data.password)) throw new Error('Contraseña incorrecta');
+  if (!await comparePassword(data.password, user.password)) throw new Error('Contraseña incorrecta');
 
   const token = jwt.sign({
     exp: Math.floor(Date.now() / 1000) + 30 * 24 * 60 * 60,
