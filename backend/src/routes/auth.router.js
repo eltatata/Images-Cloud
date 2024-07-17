9f4c8b33db4fc38395f8bb063103935f3d966cdc
eltatataa <davidtowo75@gmail.com>
2024-07-16 22:49:49 -0500

    deleted router

--- backend/src/routes/auth.router.js
+++ backend/src/routes/auth.router.js
@@ -7,6 +7,5 @@ const router = express.Router();
 
 router.post("/register", registerValidator, register);
 router.post("/login", loginValidator, login);
-router.get("/profile");
 
 export default router;
