9ef98eb7963cb47736ab115cd1b473f3cf158ff4
eltatataa <davidtowo75@gmail.com>
2024-07-17 08:47:58 -0500

    cloudinary service

---
+++ backend/src/services/cloudinary.service.js
@@ -0,0 +1,20 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+export const cloudinaryUpload = async (buffer) => {
+  const response = await new Promise((resolve, reject) => {
+    cloudinary.uploader.upload_stream({ folder: process.env.CLOUNDINARY_FOLDER }, (err, res) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(res);
+    }).end(buffer);
+  });
+
+  return response;
+}
+
+export const cloudinaryDelete = async (public_id) => {
+  await cloudinary.uploader.destroy(public_id, (error, result) => {
+    if (result.result !== "ok") throw new Error('Error al eliminar la imagen en Cloudinary');
+  });
+}
--- backend/src/services/images.service.js
+++ backend/src/services/images.service.js
@@ -1,5 +1,5 @@
 import { Image } from "../models/Image.js";
-import { v2 as cloudinary } from 'cloudinary';
+import { cloudinaryUpload, cloudinaryDelete } from "./cloudinary.service.js";
 import { NotFoundError } from '../utils/errors.manager.js';
 
 export const getImagesService = async (uid) => {
@@ -16,14 +16,7 @@ export const getImageService = async (id) => {
 export const uploadImageService = async (data) => {
   const buffer = data.image.buffer;
 
-  const cloudinaryResponse = await new Promise((resolve, reject) => {
-    cloudinary.uploader.upload_stream({ folder: process.env.CLOUNDINARY_FOLDER }, (err, res) => {
-      if (err) {
-        reject(err);
-      }
-      resolve(res);
-    }).end(buffer);
-  });
+  const cloudinaryResponse = await cloudinaryUpload(buffer);
 
   const image = new Image({
     name: data.name,
@@ -44,9 +37,7 @@ export const deleteImageService = async (id) => {
   if (!image) throw new NotFoundError('Imagen no encontrada');
 
   const { public_id } = image;
-  await cloudinary.uploader.destroy(public_id, (error, result) => {
-    if (result.result !== "ok") throw new Error('Error al eliminar la imagen en Cloudinary');
-  });
+  await cloudinaryDelete(public_id);
 
   return image;
 }
