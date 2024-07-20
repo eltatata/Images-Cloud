b3fd52c6113031adbf91645af46fbf4203a18bd0
eltatataa <davidtowo75@gmail.com>
2024-07-20 13:07:14 -0500

    logout alert

--- frontend/src/components/ButtonLogout.jsx
+++ frontend/src/components/ButtonLogout.jsx
@@ -1,27 +1,46 @@
 "use client";
 
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 import Cookies from 'js-cookie';
 import { Button } from "@nextui-org/react";
+import ModalAlert from "./ModalAlert";
 
 export default function ButtonLogout() {
   const router = useRouter();
 
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
   const handleLogout = () => {
+    setLoading(true);
+
     Cookies.remove("tokenSesionApp");
 
     router.refresh();
     router.push("/auth/login");
+
+    setLoading(false);
   }
 
   return (
-    <Button
-      variant="ghost"
-      color="secondary"
-      className="text-medium font-bold"
-      onPress={handleLogout}
-    >
-      Logout
-    </Button>
+    <>
+      <Button
+        variant="ghost"
+        color="secondary"
+        className="text-medium font-bold"
+        onPress={() => setOpen(true)}
+      >
+        Logout
+      </Button>
+      <ModalAlert
+        isOpen={open}
+        isLoading={loading}
+        onOpenChange={setOpen}
+        onConfirm={handleLogout}
+      >
+        <p>Are you sure you want to log out?</p>
+      </ModalAlert>
+    </>
   )
 }
