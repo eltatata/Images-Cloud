96af67d2005d5de1956b77e96c946154d14debb7
eltatataa <davidtowo75@gmail.com>
2024-07-27 12:32:23 -0500

    login react-hook-form

--- frontend/src/app/auth/login/page.jsx
+++ frontend/src/app/auth/login/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 import Cookies from 'js-cookie';
 import { useRouter, useSearchParams } from "next/navigation";
 import {
@@ -16,10 +17,10 @@ import {
 function LoginPage() {
   const router = useRouter();
   const searchParams = useSearchParams();
+  const { register, handleSubmit, formState: { errors } } = useForm();
   const reason = searchParams.get('reason')
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
   const [isButtonDisabled, setIsButtonDisabled] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
 
@@ -29,58 +30,86 @@ function LoginPage() {
     }
   }, [reason]);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
+  const onSubmit = handleSubmit(async (data) => {
     try {
       setIsButtonDisabled(true);
 
       const res = await fetch(process.env.NEXT_PUBLIC_API_URL + "/auth/login", {
         method: "POST",
-        body: JSON.stringify({ email, password }),
+        body: JSON.stringify({
+          email: data.email,
+          password: data.password
+        }),
         headers: {
           "Content-Type": "application/json",
         }
       });
+      const dataResponse = await res.json();
 
       if (res.ok) {
+        Cookies.set("tokenSesionApp", dataResponse.token)
+
         router.refresh();
         router.push("/profile");
         router.refresh();
+      } else {
+        setError(dataResponse.error);
       }
-
-      const data = await res.json();
-      Cookies.set("tokenSesionApp", data.token)
     } catch (error) {
       console.log(error);
     } finally {
       setIsButtonDisabled(false);
     }
-  }
+  });
 
   return (
     <>
       <div className="flex justify-between items-start w-full h-[93vh]">
         <form
           className='flex-1 space-y-6 mt-40 p-5 max-w-sm mx-auto'
-          onSubmit={handleSubmit}
+          onSubmit={onSubmit}
         >
           <div className="flex flex-col items-center gap-2">
             <h2 className='font-bold text-2xl'>Login</h2>
             <p className="text-sm text-neutral-400">Enter your email below to login to your account</p>
+            {error && (
+              <span className="p-2 rounded-lg text-red-500">{error}</span>
+            )}
           </div>
-          <Input
-            type="email"
-            label="Email"
-            variant="bordered"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <Input
-            type="password"
-            label="Password"
-            variant="bordered"
-            onChange={(e) => setPassword(e.target.value)}
-          />
+          <div className="space-y-2">
+            <Input
+              type="email"
+              label="Email"
+              variant="bordered"
+              required
+              {...register("email", {
+                required: {
+                  value: true,
+                  message: "Email is required"
+                },
+                pattern: {
+                  value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+                  message: "Invalid email",
+                },
+              })}
+            />
+            {errors.email && <p className="text-xs text-red-500">{errors.email.message}</p>}
+          </div>
+          <div className="space-y-2">
+            <Input
+              type="password"
+              label="Password"
+              variant="bordered"
+              required
+              {...register("password", {
+                required: {
+                  value: true,
+                  message: "Password is required"
+                }
+              })}
+            />
+            {errors.password && <p className="text-xs text-red-500">{errors.password.message}</p>}
+          </div>
           <Button
             type="submit"
             className="w-full"
