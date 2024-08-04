eae4f163fd8ee89cf3bd14ade52a28fc546e9d76
eltatataa <davidtowo75@gmail.com>
2024-08-03 23:32:17 -0500

    verify route

--- backend/src/routes/auth.router.js
+++ backend/src/routes/auth.router.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { login, register } from "../controllers/auth.controller.js";
+import { login, register, verify } from "../controllers/auth.controller.js";
 import { loginValidator, registerValidator } from "../utils/validator.manager.js";
 
 const router = express.Router();
 
 router.post("/register", registerValidator, register);
 router.post("/login", loginValidator, login);
+router.get("/verify/:token", verify);
 
 export default router;
