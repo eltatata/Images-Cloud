fdda2cd0e0debc13d91096eccbdca34a2fded3e3
eltatataa <davidtowo75@gmail.com>
2024-08-04 01:07:16 -0500

    email template changed

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -4,19 +4,19 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendVerificationEmail = async (email, token) => {
   const { data, error } = await resend.emails.send({
-    from: 'Acme <onboarding@resend.dev>',
+    from: 'Images Cloud <onboarding@resend.dev>',
     to: [email],
-    subject: 'Account Verification',
+    subject: 'Verificacion de Cuenta',
     html: `
-      <h1>Account Verification</h1>
-      <p>Click the link below to verify your account</p>
-      <a href="${process.env.PROD_URL}/auth/verify/${token}">Verify Account</a>
+      <h1 style="color: #333">Verificación de cuenta</h1>
+      <h2>Estimado usuario,</h2>
+      <p><b>!! Gracias !!</b> por registrarte en nuestro sitio web 😁. <br/> Para completar el proceso de verificación de tu cuenta, haz clic en el siguiente enlace ⬇️:</p>
+      <a href="${process.env.PROD_URL}/auth/verify/${token}" style="display: inline-block; padding: 10px 20px; margin: 10px 0; color: #fff; background-color: #007BFF; text-decoration: none; border-radius: 4px;">Verify Account</a>
+      <p>Si no has solicitado la creación de esta cuenta, puedes ignorar este correo.</p>
+      <p>Saludos👋,</p>
+      <p><strong>Images Cloud</strong></p>
     `
   });
 
-  if (error) {
-    return console.error({ error });
-  }
-
-  console.log({ data });
+  if (error) throw new Error(error);
 }
