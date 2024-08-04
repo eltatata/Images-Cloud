0736ea8a2730f472627700146ba4d4977e4b686a
eltatataa <davidtowo75@gmail.com>
2024-08-04 09:58:19 -0500

    verify response changed

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -25,7 +25,14 @@ export const verify = async (req, res) => {
   try {
     const token = req.params.token;
     await verifyService(token);
-    res.redirect(`${process.env.ORIGIN1}/auth/login?reason=verified`);
+    res.send(` 
+      <div style="font-family: 'Arial', sans-serif; background-color: #f4f4f4; display: flex; justify-content: center; align-items: center; height: 100vh; margin: 0;">
+        <div style="background-color: #fff; padding: 20px; border-radius: 8px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); text-align: center;">
+          <h1 style="color: #333;">Images Cloud</h1>
+          <p style="color: #666;">Tu email ha sido verificado. Puedes cerrar esta ventana.</p>
+        </div>
+      </div>
+    `);
   } catch (error) {
     handleHttp(res, error);
   }
