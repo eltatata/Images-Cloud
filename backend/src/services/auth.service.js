70577dae5a00a98cad5cbb786d88d55c518bc291
eltatataa <davidtowo75@gmail.com>
2024-08-04 00:27:09 -0500

    confirmation validation in login

--- backend/src/services/auth.service.js
+++ backend/src/services/auth.service.js
@@ -2,7 +2,7 @@ import crypto from "crypto"
 import { User } from '../models/User.js';
 import { hashPassword, comparePassword } from "../utils/password.manager.js";
 import { generateToken } from '../utils/token.manager.js';
-import { NotFoundError, BadRequestError } from '../utils/errors.manager.js';
+import { NotFoundError, BadRequestError, ForbiddenError } from '../utils/errors.manager.js';
 import { sendVerificationEmail } from "./email.service.js";
 
 export const registerService = async (data) => {
@@ -27,6 +27,7 @@ export const loginService = async (data) => {
   const user = await User.findOne({ email: data.email });
   if (!user) throw new NotFoundError('Usuario no encontrado');
   if (!await comparePassword(data.password, user.password)) throw new BadRequestError('Contraseña incorrecta');
+  if (!user.confirmed) throw new ForbiddenError('Usuario no verificado');
 
   const token = generateToken({ uid: user._id, email: user.email });
 
