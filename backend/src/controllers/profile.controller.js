0416f93130dff3338e4ef37b39d036bb2a91006e
eltatataa <davidtowo75@gmail.com>
2024-08-23 21:43:05 -0500

    updated user backend

--- backend/src/controllers/profile.controller.js
+++ backend/src/controllers/profile.controller.js
@@ -1,10 +1,21 @@
-import { deleteProfileService, getProfileSevice } from "../services/profile.service.js";
+import { deleteProfileService, getProfileSevice, updateProfileService } from "../services/profile.service.js";
 import { handleHttp } from "../utils/http.manager.js";
 
 export const getProfile = async (req, res) => {
   try {
     const uid = req.uid
     const user = await getProfileSevice(uid);
+    res.status(200).json(user);
+  } catch (error) {
+    handleHttp(res, error);
+  }
+}
+
+export const updateProfile = async (req, res) => {
+  try {
+    const uid = req.uid
+    const data = req.body
+    const user = await updateProfileService(uid, data);
     res.status(200).json(user);
   } catch (error) {
     handleHttp(res, error);
--- backend/src/routes/profile.router.js
+++ backend/src/routes/profile.router.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { deleteProfile, getProfile } from "../controllers/profile.controller.js";
+import { deleteProfile, getProfile, updateProfile } from "../controllers/profile.controller.js";
 import { requireJwt } from "../middlewares/token.middleware.js";
 
 const router = express.Router();
 
 router.get("/", requireJwt, getProfile);
+router.put("/:id", requireJwt, updateProfile);
 router.delete("/", requireJwt, deleteProfile);
 
 export default router;
